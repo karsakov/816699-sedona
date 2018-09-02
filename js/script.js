03509d195c4cd7ba89795e571d4a5d35582b7ff8
karsakov <ivankarsakov@gmail.com>
2018-09-03 00:21:17 +0300

    Финальные правки

--- js/script.js
+++ js/script.js
@@ -1,13 +1,6 @@
-var map_selector = document.querySelectorAll('.activate-booking-button');
-for ( var index = 0; index < map_selector.length; index++) {
-  map_selector[index].addEventListener('click', function () {
-  var active_map = document.querySelector('form');
-  if (active_map.classList.contains('search-form-active')) {
-  active_map.classList.remove('search-form-active');
-  }
-  else {
-  active_map.classList.add('search-form-active');
-  }
-});
-}
-
+var activate_button = document.querySelector('.activate-booking-button');
+var form = document.querySelector('.search-form');
+form.classList.remove('search-form-active');
+activate_button.addEventListener('click', function () {
+  form.classList.toggle('search-form-active')
+  })
